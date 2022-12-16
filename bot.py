e5a15e51acf9901e0551490cfb1a03cfcd8da0e8
Viljar.Slettli <Viljar.Slettli@student.uib.no>
2022-12-17 00:59:29 +0100

    Set up parse messages, intents, response

--- bot.py
+++ bot.py
@@ -6,41 +6,34 @@ from dotenv import load_dotenv
 
 import convert as c
 
-load_dotenv()
-#TOKEN = os.getenv('TOKEN') # Live convertabot
-TOKEN = os.getenv('TESTTOKEN') # Converta testbot
-
 MAX_RESPONSES = 3 # Max num of conversions the bot will do from a single message, to avoid filling up chat
-
-client = hikari.GatewayBot(token=TOKEN)
 
 errorArr = ["Invalid number and unit", "Invalid number", "Invalid unit"]
 
-class Automatic_Converter(commands.Cog):
-    def __init__(self,bot):
-        self.bot = bot
+load_dotenv()
+token = os.getenv('TOKEN') # Converta testbot
+bot = lightbulb.BotApp(token, intents = hikari.Intents(hikari.Intents.ALL_UNPRIVILEGED | hikari.Intents.MESSAGE_CONTENT))
 
-    @commands.Cog.listener()
-    async def on_message(self, ctx):
-        if ctx.author == bot.user or ctx.author.bot == True:
+@bot.listen()
+async def auto_convert(ctx: hikari.MessageCreateEvent) -> None: # Auto parse messages sent in servers
+    if not ctx.is_human:
+        return
+        
+    if re.search(r'\d', ctx.message.content): # Try converting if number found, just to trim number of messages a bit
+        try:
+            response = parseMessage(ctx.message.content,MAX_RESPONSES)
+            print('Number recognized! - ' + ctx.message.content)           
+            if len(response) == 0:
+                return 
+            await bot.rest.create_message(ctx.channel_id, response)
+            print(response)
+        except Exception as e:
+            print(f"Exception: '{e}'\nWhile parsing: '{ctx.message.content}'\n")
             return
-        elif ctx.content == "$vaffel": # Joke command as an inside joke 
-            await ctx.channel.send("Vi har ikke åpnet for bestillinger.")
-        elif re.search(r'\d', ctx.content): # Try converting if number found, just to trim number of messages a bit
-            try:
-                response = parseMessage(ctx,MAX_RESPONSES)
-                print('Number recognized! - ' + ctx.content)           
-                if len(response) == 0:
-                    return 
-                await ctx.channel.send(response)
-                print(response)
-            except Exception as e:
-                print(f"Exception: '{e}'\nWhile parsing: '{ctx.content}'\n")
-                return
 
 # Handles message parsing, calls relevant functions/modules. Returns formatted response
-def parseMessage(message, maxResponses):
-    response = c.convertHandler(message.content,maxResponses)
+def parseMessage(msg, maxResponses):
+    response = c.convertHandler(msg,maxResponses)
     if len(response) == 0:
         return response        
     fullResponse = ""
@@ -51,5 +44,4 @@ def parseMessage(message, maxResponses):
             fullResponse += r + "\n"
     return fullResponse
 
-bot.add_cog(Automatic_Converter(bot))
-bot.run(TOKEN)
+bot.run()

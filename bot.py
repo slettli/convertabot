dfb6f2836bb8cc51ec39ba2f1c7018929e124036
Viljar.Slettli <Viljar.Slettli@student.uib.no>
2022-12-16 23:59:45 +0100

    Add new imports, refer to as bot instead of client

--- bot.py
+++ bot.py
@@ -1,34 +1,28 @@
-from ast import excepthandler
 import os
-import discord
+import hikari
+import lightbulb
 import re
 from dotenv import load_dotenv
-from discord.ext import commands
 
 import convert as c
 
 load_dotenv()
-TOKEN = os.getenv('TOKEN') # Live convertabot
-#TOKEN = os.getenv('TESTTOKEN') # Converta testbot
+#TOKEN = os.getenv('TOKEN') # Live convertabot
+TOKEN = os.getenv('TESTTOKEN') # Converta testbot
 
 MAX_RESPONSES = 3 # Max num of conversions the bot will do from a single message, to avoid filling up chat
 
-#client = discord.Client()
-client = commands.Bot(command_prefix="!", help_command=None, status=discord.Status.idle)
-
-@client.event # Show initial connection
-async def on_ready():
-    print(f'{client.user} is now online!\n')
+client = hikari.GatewayBot(token=TOKEN)
 
 errorArr = ["Invalid number and unit", "Invalid number", "Invalid unit"]
 
 class Automatic_Converter(commands.Cog):
-    def __init__(self,client):
-        self.client = client
+    def __init__(self,bot):
+        self.bot = bot
 
     @commands.Cog.listener()
     async def on_message(self, ctx):
-        if ctx.author == client.user or ctx.author.bot == True:
+        if ctx.author == bot.user or ctx.author.bot == True:
             return
         elif ctx.content == "$vaffel": # Joke command as an inside joke 
             await ctx.channel.send("Vi har ikke åpnet for bestillinger.")
@@ -57,5 +51,5 @@ def parseMessage(message, maxResponses):
             fullResponse += r + "\n"
     return fullResponse
 
-client.add_cog(Automatic_Converter(client))
-client.run(TOKEN)
+bot.add_cog(Automatic_Converter(bot))
+bot.run(TOKEN)

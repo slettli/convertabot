555c684553e27983561ce6759b4e1443abc51abd
Viljar.Slettli <Viljar.Slettli@student.uib.no>
2022-12-17 01:48:52 +0100

    Add slashcommands for autoconversion, poke

--- bot.py
+++ bot.py
@@ -23,13 +23,48 @@ async def auto_convert(ctx: hikari.MessageCreateEvent) -> None: # Auto parse mes
         try:
             response = parseMessage(ctx.message.content,MAX_RESPONSES)
             print('Number recognized! - ' + ctx.message.content)           
-            if len(response) == 0:
+            if len(response) == 0: # If conversion failed
                 return 
             await bot.rest.create_message(ctx.channel_id, response)
             print(response)
         except Exception as e:
             print(f"Exception: '{e}'\nWhile parsing: '{ctx.message.content}'\n")
             return
+
+# Check if the bot is online, or just waste computing resources.
+@bot.command()
+@lightbulb.command("poke", "Poke Covertabot.")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def poke(ctx: lightbulb.Context) -> None: 
+    await ctx.respond("Stop that!")
+
+# Same as the auto parser. Will attempt to convert measurements imperial->metric or vice versa.
+@bot.command()
+@lightbulb.option("value", "Value, use . for decimal notation", float)
+@lightbulb.option("unit", "Unit to convert from", str)
+@lightbulb.command("autoconvert", "Convert a measurement to whatever Convertabot thinks is best", aliases=["autocon"])
+@lightbulb.implements(lightbulb.SlashCommand)
+async def autoconvert(ctx: lightbulb.Context) -> None:
+    number = ctx.options.value
+    unit = ctx.options.unit
+    comb = str(number) + unit
+    result = parseMessage(comb, 1)
+    await ctx.respond(result)
+
+# Convert from one unit to another one, of the user's choosing.
+@bot.command()
+@lightbulb.option("value", "Value to convert from, use . for decimal notation", float)
+@lightbulb.option("unit_1", "Unit to convert from", str)
+@lightbulb.option("unit_2", "Final unit after conversion", str)
+@lightbulb.command("convert", "Convert from one unit to another. WIP", aliases=["manualconvert"])
+@lightbulb.implements(lightbulb.SlashCommand)
+async def convert(ctx: lightbulb.Context) -> None:
+    number = ctx.options.value
+    unit = ctx.options.unit_1
+    target_unit = ctx.options.unit_2
+    comb = str(number) + unit
+    result = parseMessage(comb, 1)
+    await ctx.respond(result)
 
 # Handles message parsing, calls relevant functions/modules. Returns formatted response
 def parseMessage(msg, maxResponses):

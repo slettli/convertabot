4da1283954572e32ff1ab7be7ef3df3363d094ec
Viljar.Slettli <Viljar.Slettli@student.uib.no>
2022-12-26 04:14:17 +0100

    Make initial filtering in auto_convert more strict

--- bot.py
+++ bot.py
@@ -15,13 +15,14 @@ token = os.getenv('TOKEN')
 bot = lightbulb.BotApp(token, intents = hikari.Intents(hikari.Intents.ALL_UNPRIVILEGED | hikari.Intents.MESSAGE_CONTENT))
 start_time: datetime.datetime = datetime.datetime.now()
 
-# Parse incoming messages if auto-conversion enabled
+# Parse incoming messages if auto-conversion is enabled
 @bot.listen()
-async def auto_convert(ctx: hikari.MessageCreateEvent) -> None: # Auto parse messages sent in servers
+async def auto_convert(ctx: hikari.MessageCreateEvent) -> None:
     if not ctx.is_human:
         return 
-        
-    if re.search(r'\d', ctx.message.content): # Try converting if number found, just to trim number of messages a bit
+
+    # Try converting only if number followed by letter found, to trim number of messages parsed
+    if re.search(r"\d\s[A-Za-z]|\d[A-Za-z]", ctx.message.content): 
         try:
             response = parseMessage(ctx.message.content , MAX_RESPONSES)
 
@@ -33,18 +34,18 @@ async def auto_convert(ctx: hikari.MessageCreateEvent) -> None: # Auto parse mes
             print(f"Exception: '{e}'\nWhile parsing: '{ctx.message.content}'\n")
             return
 
-# Commands
+# Slash Commands
 
 # Check if the bot is online, or just waste computing resources.
 @bot.command()
-@lightbulb.command("poke", "Poke June.")
+@lightbulb.command("poke", "Poke June")
 @lightbulb.implements(lightbulb.SlashCommand)
 async def poke(ctx: lightbulb.Context) -> None: 
     await ctx.respond("Stop that!")
 
 # Uptime for the bot.
 @bot.command()
-@lightbulb.command("uptime", "Time since last crash.")
+@lightbulb.command("uptime", "Time since last crash")
 @lightbulb.implements(lightbulb.SlashCommand)
 async def uptime(ctx: lightbulb.Context) -> None: 
     dt = datetime.datetime.now() - start_time
@@ -57,7 +58,7 @@ async def uptime(ctx: lightbulb.Context) -> None:
 # Set a max number of conversions from a single message
 @bot.command()
 @lightbulb.option("max", "Max number of conversions from one message, 1-10", int)
-@lightbulb.command("set_max", "Set max number of conversions from one message, to avoid filling chat.")
+@lightbulb.command("set_max", "Set max number of conversions from one message, to avoid filling up chat")
 @lightbulb.implements(lightbulb.SlashCommand)
 async def set_max(ctx: lightbulb.Context) -> None: 
     if ctx.options.max > 10 or ctx.options.max < 1:
@@ -67,9 +68,9 @@ async def set_max(ctx: lightbulb.Context) -> None:
         MAX_RESPONSES = ctx.options.max
         await ctx.respond(f"The new conversions limit per message is now {MAX_RESPONSES}.")
 
-# Same as the auto parser. Will attempt to convert measurements imperial -> metric or vice versa.
+# Same as the auto parser. Will attempt to convert measurements between imperial <-> metric.
 @bot.command()
-@lightbulb.option("value", "Value to convert, use . for decimal notation", float)
+@lightbulb.option("value", "Value to convert (use . for decimal notation)", float)
 @lightbulb.option("unit", "Unit to convert from", str)
 @lightbulb.command("convert", "Convert a measurement to whatever June thinks is best", aliases=["autocon","autoconvert"])
 @lightbulb.implements(lightbulb.SlashCommand)
@@ -80,14 +81,14 @@ async def convert(ctx: lightbulb.Context) -> None:
     result = parseMessage(comb, 1)
     await ctx.respond(result)
 
-# Convert one unit to another unit, user's choice.
+# Convert one unit to another unit, user's choice. Not enabled or completed.
 #@bot.command()
-@lightbulb.option("value", "Value to convert, use . for decimal notation", float)
-@lightbulb.option("unit_1", "Unit to convert FROM", str)
-@lightbulb.option("unit_2", "Final unit after conversion", str)
-@lightbulb.command("manualconvert", "Convert from one unit to another.", aliases=["mancon","manualcon"])
+@lightbulb.option("value", "Value to convert (use . for decimal notation)", float)
+@lightbulb.option("unit_1", "Unit to convert from", str)
+@lightbulb.option("unit_2", "Target unit", str)
+@lightbulb.command("manual_convert", "Convert from one unit to another (if possible)", aliases=["mancon","manualcon"])
 @lightbulb.implements(lightbulb.SlashCommand)
-async def manualconvert(ctx: lightbulb.Context) -> None:
+async def manual_convert(ctx: lightbulb.Context) -> None:
     number = ctx.options.value
     unit = ctx.options.unit_1
     target_unit = ctx.options.unit_2
